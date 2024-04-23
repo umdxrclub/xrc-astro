5a93859fef3f259379b9a06d27f8c2b0926926bd
D.J. Figueroa <djfigs10@gmail.com>
2024-04-22 22:35:59 -0400

    Sort events by date/time

--- src/util/xrc-suite.ts
+++ src/util/xrc-suite.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import process from "process";
-import type { Event, Website } from "../types/PayloadSchema";
+import type { Article, Event, Website } from "../types/PayloadSchema";
 
 dotenv.config();
 
@@ -10,7 +10,6 @@ export function xrcPath(path: string) {
 
   return url;
 }
-
 
 export async function fetchXRCApi(path: string) {
   if (path.startsWith("/")) path = path.substring(1);
@@ -28,9 +27,22 @@ export async function fetchXRCApi(path: string) {
 
 export async function fetchEvents(): Promise<Event[]> {
   let res = await fetchXRCApi("/events");
-  let events = res.docs as Event[]
-  
+  let events = res.docs as Event[];
+  let eventDates = new Map<string, Date>();
+  events.forEach((e) => {
+    eventDates.set(e.id, new Date(e.startDate));
+  });
+
+  events.sort(
+    (a, b) => eventDates.get(b.id)!.getTime() - eventDates.get(a.id)!.getTime()
+  );
+
   return events;
+}
+
+export async function fetchArticle(id: string): Promise<Article> {
+  let article = (await fetchXRCApi("/articles/" + id)) as Article;
+  return article;
 }
 
 var website: Website | undefined;
