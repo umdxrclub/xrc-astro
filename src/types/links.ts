c6ceaea1dd2f841862d964fbbd1657089e33810a
D.J. Figueroa <djfigs10@gmail.com>
2024-09-10 17:20:49 -0400

    Clean up event cards

---
+++ src/types/links.ts
@@ -0,0 +1,3 @@
+export const DiscordInviteUrl = "https://discord.gg/mn8ZKTwXFQ";
+export const TerpLinkUrl = "https://terplink.umd.edu/organization/xr-club";
+export const GoogleCalendarUrl = "https://calendar.google.com/calendar/embed?src=kdeu518ej1b56il3ugn9hcl6b0%40group.calendar.google.com&ctz=America%2FNew_York";
